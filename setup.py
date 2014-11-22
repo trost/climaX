18ffc5607ee2bd98f5f5e56bca6fd8678aa5605b
Arne Neumann <github@arne.cl>
2014-11-23 00:01:27 +0100

    added minimal setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(name='climaX',
+      version='0.1.1',
+      description='Collection for processing climate data for agricultural studies',
+      long_description=README,
+      author='Christian Schudoma',
+      author_email='',
+      url='https://github.com/arne-cl/climaX',
+      py_modules=['getClimateData', 'vpd_heatsum', 'queries', 'login'],
+      scripts=['getClimateData.py'],
+      license='MIT License',
+      #install_requires=['MySQLdb'],
+     )
+
+
