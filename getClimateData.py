5e1a7bcf8c4751de4d81e6e920bee44d13f619c1
Arne Neumann <climax.programming@arne.cl>
2014-11-22 20:29:06 +0100

    updated getClimateData module (Heike's version from 2014-10-20)

--- getClimateData.py
+++ getClimateData.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import sys
-sys.path.append('/home/schudoma/projects/trost/QSolanum')
-sys.path.append('/home/schudoma/projects/trost/QSolanum/climaX')
+#sys.path.append('/home/schudoma/projects/trost/QSolanum')
+#sys.path.append('/home/schudoma/projects/trost/QSolanum/climaX')
+
+sys.path.append('d:/work/TROST/Climate_Model')
 
 from itertools import starmap
-from datetime import datetime
+from datetime import datetime, date
 
 import os 
 import pickle
@@ -15,39 +17,13 @@ import login
 DB = login.get_db()
 C = DB.cursor()
 
-
-
-CLIMATE_QUERY1 = """
-SELECT 
-DATE(T.datum), 
-T.tmin, 
-T.tmax, 
-P.amount,
-SUM(SC.amount)
-FROM temperatures T 
-JOIN cultures C ON C.location_id = T.location_id 
- AND year(C.planted) = 2012 
-JOIN solarCalc_hourlySolarRadiation SC ON DATE(SC.datum) = T.datum
- AND SC.location_id = T.location_id
- AND year(SC.datum) = 2012
-LEFT OUTER JOIN precipitation P ON P.datum = T.datum 
- AND P.location_id = T.location_id 
-WHERE C.id = 56878
- AND year(T.datum) = 2012 
- AND T.datum < '2012-06-08' 
- AND (T.datum >= C.planted - interval 14 day) 
- AND T.datum <= C.terminated 
-GROUP BY DATE(T.datum)
-ORDER BY T.datum;
-""".strip().replace('\n', '')
-
 PREC_QUERY = """
 SELECT 
 DATE(P.datum),
 P.amount
 FROM precipitation P 
 JOIN cultures C ON P.location_id = C.location_id 
-WHERE C.id = 56878 
+WHERE C.id = %(CULTURE_ID)i
 ORDER BY P.datum; 
 """.strip().replace('\n', ' ')
 
@@ -57,53 +33,82 @@ DATE(I.datum),
 I.amount,
 I.treatment_id
 FROM irrigation I
-WHERE I.culture_id = 56878 
+WHERE I.culture_id = %(CULTURE_ID)i
 ORDER BY I.datum;
 """.strip().replace('\n', ' ')
 
-
-
-CLIMATE_QUERY2 = """
-SELECT 
-TAHV.datum, 
-TAHV.amount,
-FFHM.amount,
-UUHV.amount
-FROM cultures C 
-JOIN usesWeatherStation uWS ON uWS.location_id = C.location_id 
- AND C.id = 56878
-JOIN dwd_hourlyAirTemperature_TAHV TAHV ON TAHV.station_id = uWS.station_id
- AND year(TAHV.datum) = year(C.planted)
-JOIN dwd_hourlyMeanWindspeed_FFHM FFHM ON FFHM.station_id = uWS.station_id
- AND FFHM.datum = TAHV.datum
-JOIN dwd_hourlyRelHumidity_UUHV UUHV ON UUHV.station_id = uWS.station_id
- AND UUHV.datum = TAHV.datum
-WHERE C.id = 56878
- AND year(TAHV.datum) = 2012 
- -- AND TAHV.datum < '2012-06-08' 
- AND (TAHV.datum >= C.planted + interval 14 day) 
- AND TAHV.datum <= C.terminated 
-GROUP BY TAHV.datum
-ORDER BY TAHV.datum;
+FAST_CLIMATE_QUERY = """
+SELECT
+wind.date1, temperature, windspeed, relHumidity
+FROM cultures C
+inner join 
+(select C.id as C1, FFHM.datum as date1, FFHM.amount as windspeed
+from dwd_hourlyMeanWindspeed_FFHM FFHM
+left join usesWeatherStation uWS on uWS.station_id = FFHM.station_id and uWS.stationData = 'FFHM'
+left join cultures C on C.location_id = uWS.location_id
+where C.id = %(CULTURE_ID)i
+and (FFHM.datum >= C.planted + interval 14 day) 
+and FFHM.datum < C.terminated
+ORDER BY FFHM.datum) wind
+on C.id = wind.C1
+left join
+(select C.id as C2, TAHV.datum as date2, TAHV.amount as temperature
+from dwd_hourlyAirTemperature_TAHV TAHV
+left join usesWeatherStation uWS on uWS.station_id = TAHV.station_id and uWS.stationData = 'TAHV'
+left join cultures C on C.location_id = uWS.location_id
+where C.id = %(CULTURE_ID)i
+and (TAHV.datum >= C.planted + interval 14 day) 
+and TAHV.datum < C.terminated
+ORDER BY TAHV.datum) temp
+on wind.date1 = temp.date2
+left join
+(select C.id as C3, UUHV.datum as date3, UUHV.amount as relHumidity
+from dwd_hourlyRelHumidity_UUHV UUHV
+left join usesWeatherStation uWS on uWS.station_id = UUHV.station_id and uWS.stationData = 'UUHV'
+left join cultures C on C.location_id = uWS.location_id
+where C.id = %(CULTURE_ID)i
+and (UUHV.datum >= C.planted + interval 14 day) 
+and UUHV.datum < C.terminated
+ORDER BY UUHV.datum) hum
+on temp.date2 = hum.date3;
 """.strip().replace('\n', ' ')
 
+DAYLIGHT_QUERY = """
+SELECT 
+sC.datum,
+sC.amount
+FROM solarCalc_hourlySolarRadiation sC
+JOIN cultures C ON C.location_id = sC.location_id
+WHERE C.id = %(CULTURE_ID)i
+AND (sC.datum >= C.planted + interval 14 day)
+AND (sC.datum < C.terminated)
+ORDER BY sC.datum
+""".strip().replace('\n', ' ')
 
-
-def calculateTemperatureStress(rawData, tub=30.0, tlb=8.0):
+def calculateLightIntensity(rawData, flowerDate='2012-07-01'):
+    L1, L2 = [], []
+    dates = [row[0].date() for row in rawData]
+    fDate = map(int, flowerDate.split('-'))
+    fDate = date(fDate[0], fDate[1], fDate[2])
     
-    stressScore = lambda x:len(x) * sum(x)/len(x)
+    dailyLight = {date_: [] for date_ in set(dates)}
+    for row in rawData:
+        if row[1] > 0.0:
+            dailyLight[row[0].date()].append(row[1])
 
-    coldDays, hotDays = [], []
-    loTemp, hiTemp = tlb, tub
-    for day in rawData:        
-        if day[1] < tlb:
-            coldDays.append(abs(tlb - day[1]))
-            loTemp = min(day[1])
-        elif day[2] > tub:
-            hotDays.append(abs(day[2] - tub))
-            hiTemp = max(day[2])
-        pass
-    return stressScore(coldDays), stressScore(hotDays)
+
+    L1, L2 = [], []
+    for day in dailyLight:
+        if day < fDate:
+            L1.append(sum(dailyLight[day]) * len(dailyLight[day]))
+        else:
+            L2.append(sum(dailyLight[day]) * len(dailyLight[day]))
+
+    return sum(L1) / len(L1), sum(L2) / len(L2)
+
+    
+
+
 
 def calculateSoilWater(precipitation, evaporation,  
                        soilVolume, availMoistCap, irrigation=dict()):  
@@ -118,30 +123,74 @@ def calculateSoilWater(precipitation, evaporation,
 
         waterGain = precipitation.get(date_, [0.0])[0] + irrigation.get(date_, [0.0])[0]
         soilWater.append(min(soilVolume, waterGain + soilWater[-1]))
-        
-        #initSoilWater += precipitation.get(date_, [0.0])[0]
-        #initSoilWater += irrigation.get(date_, [0.0])[0]
-        
-
-
-    #initSoilWater = min(soilVolume, initSoilWater)
-    #soilWater.append(initSoilWater)
     
     # From day 14 on calculate net water = soilWater from day before + evaporation loss + water gain
     for date_ in dates[14:]:
         evaporationLoss = evaporation.get(date_, 0.0)
-        # print precipitation.get(date_, 0.0), irrigation.get(date_, [0.0])[0]
-
         waterGain = precipitation.get(date_, [0.0])[0] + irrigation.get(date_, [0.0])[0]  
-        # netwater = (soilVolume, last computed soilwater + evaporation loss + water gain)
-        netWater = soilWater[-1] + evaporationLoss + waterGain  #soilVolume,  
-
-
+        netWater = soilWater[-1] + evaporationLoss + waterGain
         soilWater.append(max(min(netWater, soilVolume), 0))
 
-    # del soilWater[0]
-    # return dict(zip(dates[14:], soilWater[1:]))
     return dict(zip(dates, soilWater[1:]))
+
+        
+def calculateTempStressDays(rawData, tub=30.0, tlb=8.0, flowerDate='2012-07-01'):
+
+    stressScore = lambda x:len(x) * sum(x)/len(x)
+
+    hotDays, coldDays = [], []
+    dates = [row[0].date() for row in rawData]
+    fDate = map(int, flowerDate.split('-'))
+    print fDate
+    fDate = date(fDate[0], fDate[1], fDate[2])
+    dailyMinMaxTemp = {date_: [1000.0, -1000.0] for date_ in set(dates)}
+    for row in rawData:
+        date_, temp = row[0].date(), row[1]
+        dailyMinMaxTemp[date_] = [min(temp, dailyMinMaxTemp[date_][0]),
+                                  max(temp, dailyMinMaxTemp[date_][1])]
+    C1, C2, H1, H2 = [], [], [], []
+    for day in dailyMinMaxTemp:
+        tMin, tMax = dailyMinMaxTemp[day]
+        coldStress, heatStress = tMin < tlb, tMax > tub
+        
+        if day < fDate:
+            if coldStress:
+                C1.append(abs(tlb - tMin))
+            if heatStress:
+                H1.append(abs(tMax - tub))
+        else:
+            if coldStress:
+                C2.append(abs(tlb - tMin))
+            if heatStress:
+                H2.append(abs(tMax - tub))
+        
+    return tuple(map(stressScore, [C1, C2, H1, H2]))
+            
+
+def calculateDroughtStressDays(rawData,
+                               soilVolume, availMoistCap,
+                               precipitation, irrigation,
+                               stressThreshold=10.0,
+                               flowerDate='2012-07-01'):
+    evaporation = calculateEvaporation(rawData)
+    
+    soilWater = calculateSoilWater(precipitation, evaporation, 
+                                   soilVolume, availMoistCap, 
+                                   irrigation=irrigation)
+    fDate = map(int, flowerDate.split('-'))
+    fDate = date(fDate[0], fDate[1], fDate[2])
+    W1, W2 = 0, 0
+    for day in soilWater:
+        if soilWater[day] < stressThreshold:
+            if day < fDate:
+                W1 += 1
+            else:
+                W2 += 1
+
+    return W1, W2
+
+            
+    
 
 
 def calculateEvaporation(rawData):
@@ -180,37 +229,49 @@ def calculateEvaporation(rawData):
             for date_ in dates}
 
 def main(argv):
+
+    cultureID = 56878
+    floweringDate = '2012-07-01'
     
     # Parameters for Area 5544 (Atting), ooo spooky :O
     soilVolume, availMoistCap = 42, 0.14
 
+    """
+    Script needs to be called from command line for each culture
+    
+    cultureID = argv[0]
+    floweringDate = argv[1]
+    soilVolume = argv[2]
+    availMoistCap = argv[3]
+    """
 
-    C.execute(PREC_QUERY)
+    C.execute(PREC_QUERY % {'CULTURE_ID': cultureID})
     precipitation = dict(map(lambda x:(x[0], x[1:]), 
                              [row for row in C.fetchall()]))
-    C.execute(IRRI_QUERY)
+    C.execute(IRRI_QUERY % {'CULTURE_ID': cultureID})
     irrigation = dict(map(lambda x:(x[0].date(), x[1:]), 
                           [row for row in C.fetchall()]))
 
-    if not os.path.exists('evaporation.dat'):
-        print 'de novo'
-        C.execute(CLIMATE_QUERY2)
 
-        evaporation = calculateEvaporation([row for row in C.fetchall()])        
-        pickle.dump(evaporation, open('evaporation.dat', 'wb'))
-    else:
-        evaporation = pickle.load(open('evaporation.dat'))
+    C.execute(FAST_CLIMATE_QUERY % {'CULTURE_ID': cultureID})
+    climateData = [row for row in C.fetchall()]
+
+    tempStressDays = calculateTempStressDays(climateData,
+                                             flowerDate=floweringDate)    
+    droughtStressDays = calculateDroughtStressDays(climateData,
+                                                   soilVolume, availMoistCap,
+                                                   precipitation, irrigation,
+                                                   stressThreshold=10.0,
+                                                   flowerDate=floweringDate)
+
+    C.execute(DAYLIGHT_QUERY % {'CULTURE_ID': cultureID})
+    lightData = [row for row in C.fetchall()]
+
+    lightIntensity = calculateLightIntensity(lightData, 
+                                             flowerDate=floweringDate)
 
 
-        
-    soilWater = calculateSoilWater(precipitation, evaporation, 
-                                   soilVolume, availMoistCap, 
-                                   irrigation=irrigation)
-
-    
-
-    for date_ in sorted(soilWater):
-        print date_, soilWater[date_]
+    print tempStressDays + droughtStressDays + lightIntensity
 
 
     
