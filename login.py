44be6db66a1bbf63a90a741eb099785e019d8627
Arne Neumann <github@arne.cl>
2014-11-22 23:06:36 +0100

    prepared login module for real mysql connection

--- login.py
+++ login.py
@@ -3,14 +3,22 @@
 
 """This is a mock replacement of the missing login module"""
 
+import MySQLdb
+
 
 def get_db():
     return FakeDatabaseObject()
 
 
+def get_real_db(host="localhost", user="john", passwd="megajonhy",
+                db="jonhydb"):
+    return MySQLdb.connect(host, user, passwd, db)
+
+
 class FakeDatabaseObject(object):
     def __init__(self):
         pass
+
     def cursor(self):
         return FakeCursor()
 
@@ -18,9 +26,9 @@ class FakeDatabaseObject(object):
 class FakeCursor(object):
     def __init__(self):
         pass
+
     def execute(self, arg1):
         pass
+
     def fetchall(self):
         return []
-
-
