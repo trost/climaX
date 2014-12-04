bee7ec79fff90cb0af1ee84dbd63a8e523ccb56d
Arne Neumann <github@arne.cl>
2014-12-04 21:45:57 +0100

    get_drought_stress_days() now handles control/stress treatment separately

--- getClimateData.py
+++ getClimateData.py
@@ -3,11 +3,16 @@
 import sys
 from itertools import starmap
 from datetime import date
+from collections import defaultdict
 import argparse
 
 from vpd_heatsum import calc_VPD
 from queries import PREC_QUERY, IRRI_QUERY, FAST_CLIMATE_QUERY, DAYLIGHT_QUERY
 import login  # TODO: get the real login module from Christian, this is fake
+
+# treatment IDs
+CONTROL = 169
+STRESS = 170
 
 
 def datestring2object(datestring):
@@ -178,26 +183,56 @@ def get_drought_stress_days(rawData, soilVolume, availMoistCap, precipitation,
     calculates the number of drought stress days before and after the flowering
     date.
 
+    Parameters
+    ----------
+    rawData : list of (datetime.datetime, float, float, float) tuples
+        ???
+    soilVolume : float
+        soil volume
+    availMoistCap : float
+        ???
+    precipitation : dict, key = datatime.date, value = float
+        ???
+    irrigation : dict, key = datetime.date, value = list of (float, long) tuples
+        maps from a date to a list of (irrigation amount, treatment_id) tuples.
+        The treatment ID is either 169 (control group) or 170 (stress).
+    stressThreshold : float
+        ???
+    flowerDate : str
+        flowering date in YYYY-MM-DD format
+
     Returns
     -------
-    droughtStressDays : 2-tuple of int
-        number of drought stress days (before flowering, after flowering),
-        e.g. (16, 0)
+    droughtStressDays : (int, int) or ((int, int) (int, int))
+        Iff ``irrigation`` is empty: number of drought stress days (DSDs)
+        (before flowering, after flowering), e.g. (16, 0).
+        Otherwise: ((control DSDs before, control DSDs after),
+        (stress DSDs before, stress DSDs after)).
     """
-    evaporation = get_evaporation(rawData)
+    def stress_days(soil_water, flowering_date, stressThreshold):
+        stress_days_before, stress_days_after = 0, 0
+        for day in soil_water:
+            if soil_water[day] < stressThreshold:
+                if day < flowering_date:
+                    stress_days_before += 1
+                else:  # day >= flowering_date
+                    stress_days_after += 1
+        return stress_days_before, stress_days_after
 
-    soilWater = get_soil_water(precipitation, evaporation, soilVolume,
-                           availMoistCap, irrigation=irrigation)
+    evaporation = get_evaporation(rawData)
     flowering_date = datestring2object(flowerDate)
 
-    W1, W2 = 0, 0
-    for day in soilWater:
-        if soilWater[day] < stressThreshold:
-            if day < flowering_date:
-                W1 += 1
-            else:  # day >= flowering_date
-                W2 += 1
-    return W1, W2
+    if irrigation:
+        control, stress = get_soil_water(precipitation, evaporation,
+                                         soilVolume, availMoistCap, irrigation)
+        return stress_days(control, flowering_date, stressThreshold), \
+            stress_days(stress, flowering_date, stressThreshold)
+
+    else:
+        soil_water = get_soil_water(precipitation, evaporation, soilVolume,
+                                    availMoistCap, irrigation)
+        return stress_days(soil_water, flowering_date, stressThreshold)
+
 
 
 def get_evaporation(rawData):
@@ -269,8 +304,10 @@ def main(cursor, cultureID=56878, floweringDate='2012-07-01', soilVolume=42,
     precipitation = {date: precip for (date, precip) in cursor.fetchall()}
 
     cursor.execute(IRRI_QUERY % {'CULTURE_ID': cultureID})
-    irrigation = dict(map(lambda x: (x[0], x[1:]),
-                          [row for row in cursor.fetchall()]))
+    irrigation = defaultdict(list)
+    # for some days, there are two rows (stress vs. control)
+    for date, irri_amount, treatment_id in cursor.fetchall():
+        irrigation[date].append( (irri_amount, treatment_id) )
 
     cursor.execute(FAST_CLIMATE_QUERY % {'CULTURE_ID': cultureID})
     climateData = [row for row in cursor.fetchall()]
