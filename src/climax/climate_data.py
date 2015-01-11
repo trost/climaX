9413d43d9bea641942ee0e10da51c9fc9e3e7648
Arne Neumann <github@arne.cl>
2015-01-11 23:57:00 +0100

    moved yesterdays_soil_value()

--- src/climax/climate_data.py
+++ src/climax/climate_data.py
@@ -136,6 +136,35 @@ def get_light_intensity(light_data, flowerDate='2012-07-01'):
     return sum(L1) / len(L1), sum(L2) / len(L2)
 
 
+def yesterdays_soil_value(soil_water, day, treatment):
+    """
+    given a day, returns the soil water value of the previous day for
+    a given treatment.
+
+    Parameters
+    ----------
+    soil_water : defaultdict of defaultdict(float)
+        maps from a datetime.date to a dict two one or two treatments
+        ('control' and 'stress'). a treatment maps to the soil water
+        content (on the given day and with the given treatment).
+    day : datetime.date
+        a day during the field trial
+    treatment : str
+        'control' or 'stress'
+
+    Returns
+    -------
+    soil_value : float
+        soil water value of the previous day under the given treatment,
+        defaults to 0.0
+    """
+    yesterday = datetime.date.fromordinal(day.toordinal()-1)
+    if yesterday in soil_water:
+        return soil_water[yesterday].get(treatment, 0.0)
+    else:
+        return 0.0
+
+
 def get_soil_water(trial_dates, precipitation, evaporation, soilVolume,
                    availMoistCap, irrigation=dict()):
     """
@@ -168,34 +197,6 @@ def get_soil_water(trial_dates, precipitation, evaporation, soilVolume,
         ('control' and 'stress'). a treatment maps to the soil water content
         (on the given day and with the given treatment).
     """
-    def yesterdays_soil_value(soil_water, day, treatment):
-        """
-        given a day, returns the soil water value of the previous day for
-        a given treatment.
-
-        Parameters
-        ----------
-        soil_water : defaultdict of defaultdict(float)
-            maps from a datetime.date to a dict two one or two treatments
-            ('control' and 'stress'). a treatment maps to the soil water
-            content (on the given day and with the given treatment).
-        day : datetime.date
-            a day during the field trial
-        treatment : str
-            'control' or 'stress'
-
-        Returns
-        -------
-        soil_value : float
-            soil water value of the previous day under the given treatment,
-            defaults to 0.0
-        """
-        yesterday = datetime.date.fromordinal(day.toordinal()-1)
-        if yesterday in soil_water:
-            return soil_water[yesterday].get(treatment, 0.0)
-        else:
-            return 0.0
-
     treatments = ('control', 'stress')
     soil_water = defaultdict(lambda : defaultdict(float))
 
